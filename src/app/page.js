987a0b236ee073febba04dea2f6f2abd4a27af3f
Manik Lakhanpal <maniklakhanpal@Maniks-MacBook-Pro.local>
2024-10-19 23:01:24 +0530

    Content Page was added

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import NavigationBar from "@/components/navigationBar";
 import HeroPage from "@/containers/HeroPage";
+import ContentPage from "@/containers/contentPage";
 
 export default function Home() {
   return(
     <div>
       <NavigationBar />
       <HeroPage />
+      <ContentPage />
     </div>
   );
 }
